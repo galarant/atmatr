5f5a40ec313f66887d7b4ce2b985489a3898ca6f
Jeff <jeff.revesz@buzzfeed.com>
2014-05-26 13:57:08 -0400

    styles and more reg stuff

--- atmatr/apps/frontend/views.py
+++ atmatr/apps/frontend/views.py
@@ -1,11 +1,27 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.template.loader import get_template
+from django.template import Context
+from django.core.mail import send_mail
+
+from registration.backends.simple.views import RegistrationView
 
 
 class IndexView(TemplateView):
 
     template_name = 'index.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
-        return context
+
+class ActivationView(RegistrationView):
+
+    """
+    Handles special behavior on registration, like sending email and setting the cookie
+    """
+
+    def post(self, request, **kwargs):
+        send_mail(subject=get_template('activation_email_subject.txt').render(Context({})).strip(),
+                  message=get_template('activation_email.txt').render(Context(request.POST)),
+                  from_email=settings.SERVER_EMAIL,
+                  recipient_list=[request.POST['email']])
+        return super(ActivationView, self).post(request, **kwargs)
