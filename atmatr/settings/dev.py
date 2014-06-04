ebda4326263cd9d491bafbc9778a51908d00383f
Jeff <jeff.revesz@buzzfeed.com>
2014-06-03 20:40:15 -0400

    added logout button

--- atmatr/settings/dev.py
+++ atmatr/settings/dev.py
@@ -83,18 +83,3 @@ LOGGING['loggers']['atmatr'] = {
     'handlers': ['console'],
     'level': 'DEBUG',
 }
-
-#================== DEBUG TOOLBAR SETTINGS =================
-INTERNAL_IPS = ('127.0.0.1',)
-
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
-)
